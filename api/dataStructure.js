43e762248d7134b1a1ed75ac950bc7d86266b187
mjr7121-sketch <mjr7121@psu.edu>
2026-04-05 20:30:20 -0400

    project one

--- api/dataStructure.js
+++ api/dataStructure.js
@@ -7,7 +7,14 @@ export default function handler(req, res) {
       "location": "Reading, PA",
       "pic": "https://www.thesprucepets.com/thmb/Q6vj0wjZpuDir35ywDO3Ke4K3CA=/1500x0/filters:no_upscale():strip_icc()/shih-tzu-dog-breed-profile-1117999-hero-5541b7f6f936478ca766d85ff5af202e.jpeg",
       "datePosted": "posted 1 day ago",
-      "caption": "a Shih Tzu staring"
+      "caption": "a Shih Tzu staring",
+
+      "author": {
+      "name": "ShihTzu2016",
+      "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEU8M1XxZ2nie8BFukvNY3qA3sBwikITJP8g&s",
+      "userSince": "2016",
+      "channel": "dogs"
+      }
   },
   {
       "userName": "GoldenRetrieverLover101",
@@ -16,7 +23,14 @@ export default function handler(req, res) {
       "location": "Forest City, PA",
       "pic": "https://t3.ftcdn.net/jpg/01/72/07/44/360_F_172074409_hADHhLMTjELdx0cMkycnzYFZZZz4OYbv.jpg",
       "datePosted": "posted 2 days ago",
-      "caption": "a Golden Retriever outside"
+      "caption": "a Golden Retriever outside",
+
+      "author": {
+      "name": "GoldenRetrieverLover101",
+      "image": "https://www.petplace.com/article/breed/media_1e93c4344f5a91e8440bd4f9e5a6a9a1a9b57e1f6.jpeg?width=450&format=jpeg&optimize=medium",
+      "userSince": "2013",
+      "channel": "dogs"
+      }
   },
   {
       "userName": "GoldenDoodle45947",
@@ -25,7 +39,14 @@ export default function handler(req, res) {
       "location": "Cape May, NJ",
       "pic": "https://www.purina.co.uk/sites/default/files/styles/square_medium_440x440/public/2022-07/Goldendoodle-image-2.jpg?h=cb57b42f&itok=2on7elHc",
       "datePosted": "posted 5 days ago",
-      "caption": "a Golden Doodle playing ball"
+      "caption": "a Golden Doodle playing ball",
+
+      "author": {
+      "name": "GoldenDoodle45947",
+      "image": "https://www.ellevetsciences.com/wp-content/uploads/2022/03/image-4-1024x717.jpeg",
+      "userSince": "2025",
+      "channel": "dogs"
+      }
 
   },
   {
@@ -35,7 +56,14 @@ export default function handler(req, res) {
       "location": "Virginia Beach, VA",
       "pic": "https://www.petplace.com/article/dogs/breeds/dog-breeds/media_1e51607bf5488af3f6af7efbc0ac26be624866a24.jpeg?width=750&format=jpeg&optimize=medium",
       "datePosted": "posted 7 days ago",
-      "caption": "a Bulldog running!!"
+      "caption": "a Bulldog running!!",
+
+      "author": {
+      "name": "BulldogLover24334",
+      "image": "https://cdn.britannica.com/07/234207-050-0037B589/English-bulldog-dog.jpg",
+      "userSince": "2017",
+      "channel": "dogs"
+      }
 
   },
     {
@@ -45,7 +73,14 @@ export default function handler(req, res) {
       "location": "Ocean City, MD",
       "pic": "https://t3.ftcdn.net/jpg/01/77/68/20/360_F_177682035_u0Sb7eDy2Zpzmlhpnw781kjCFU1suY6f.jpg",
       "datePosted": "posted 2 days ago",
-      "caption": "a Husky on a rock"
+      "caption": "a Husky on a rock",
+
+      "author": {
+      "name": "Husky24334",
+      "image": "https://t3.ftcdn.net/jpg/01/00/80/08/360_F_100800827_RbUfH5xgkYCj8GKMaPi4d9laRo1rRGEi.jpg",
+      "userSince": "2019",
+      "channel": "dogs"
+      }
 
   },
     {
@@ -55,7 +90,14 @@ export default function handler(req, res) {
       "location": "Richmond, VA",
       "pic": "https://cdn.pixabay.com/photo/2017/02/03/17/34/pug-2035675_1280.jpg",
       "datePosted": "posted 4 days ago",
-      "caption": "a pug on grass"
+      "caption": "a pug on grass",
+
+      "author": {
+      "name": "adventerouspug24334",
+      "image": "https://upload.wikimedia.org/wikipedia/commons/7/7f/Pug_portrait.jpg",
+      "userSince": "2021",
+      "channel": "dogs"
+      }
 
   },
    {
@@ -65,7 +107,14 @@ export default function handler(req, res) {
       "location": "Pennsylvania",
       "pic": "https://pet-health-content-media.chewy.com/wp-content/uploads/2024/09/11175748/202105Dashund-1201198563-scaled-1.jpg",
       "datePosted": "posted 1 day ago",
-      "caption": "a Dachshund on a log"
+      "caption": "a Dachshund on a log",
+
+      "author": {
+      "name": "Dachshundfan114",
+      "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFwYMga4Ro96gjimyR34HdFPPjKL1mr-keqg&s",
+      "userSince": "2023",
+      "channel": "dogs"
+      }
 
   },
      {
@@ -75,7 +124,14 @@ export default function handler(req, res) {
       "location": "Boston, MA",
       "pic": "https://t4.ftcdn.net/jpg/01/15/42/25/360_F_115422554_xm6JYj6PXbbIplilYpqzi9BSw7bCDvu1.jpg",
       "datePosted": "posted 4 days ago",
-      "caption": "Pomeranian on a rock"
+      "caption": "Pomeranian on a rock",
+
+      "author": {
+      "name": "Pomeranian43",
+      "image": "https://t4.ftcdn.net/jpg/01/15/42/25/360_F_115422554_xm6JYj6PXbbIplilYpqzi9BSw7bCDvu1.jpg",
+      "userSince": "2024",
+      "channel": "dogs"
+      }
 
   },
      {
@@ -85,7 +141,15 @@ export default function handler(req, res) {
       "location": "California",
       "pic": "https://d128mjo55rz53e.cloudfront.net/media/images/yorkshire-10.max-400x400.format-jpeg.jpg",
       "datePosted": "posted 2 days ago",
-      "caption": "a yorkie outside"
+      "caption": "a yorkie outside",
+
+
+      "author": {
+      "name": "yorkielover112",
+      "image": "https://www.sheknows.com/wp-content/uploads/2018/12/gggk1jzcofgkh8ntt6rz.jpeg?w=60",
+      "userSince": "2014",
+      "channel": "dogs"
+      }
 
   },
      {
@@ -95,7 +159,14 @@ export default function handler(req, res) {
       "location": "Miami, Florida",
       "pic": "https://www.akc.org/wp-content/uploads/2017/11/Australian-Shepherd.1.jpg",
       "datePosted": "posted 4 days ago",
-      "caption": "playing with a mini basketball"
+      "caption": "playing with a mini basketball",
+
+      "author": {
+      "name": "australianshepherdfan3",
+      "image": "https://showsightmagazine.com/wp-content/uploads/2023/12/Head.jpg",
+      "userSince": "2017",
+      "channel": "dogs"
+      }
 
   },
      {
@@ -105,7 +176,14 @@ export default function handler(req, res) {
       "location": "North Carolina",
       "pic": "https://www.shutterstock.com/image-photo/beagle-dog-trotting-on-grass-600nw-2481313271.jpg",
       "datePosted": "posted 4 days ago",
-      "caption": "beagle on a run"
+      "caption": "beagle on a run",
+
+      "author": {
+      "name": "beagleowner457",
+      "image": "https://d128mjo55rz53e.cloudfront.net/media/images/blog-breed-beagle_2.max-400x400.format-jpeg.jpg",
+      "userSince": "2023",
+      "channel": "dogs"
+      }
 
   },
      {
@@ -115,7 +193,14 @@ export default function handler(req, res) {
       "location": "Connecticut",
       "pic": "https://pet-health-content-media.chewy.com/wp-content/uploads/2024/09/11170901/202106GettyImages-1483086603-scaled-1.jpg",
       "datePosted": "posted 4 days ago",
-      "caption": "Cairn Terrier with a ball"
+      "caption": "Cairn Terrier with a ball",
+
+      "author": {
+      "name": "CairnTerrier7458",
+      "image": "https://pet-health-content-media.chewy.com/wp-content/uploads/2024/09/11170901/202106GettyImages-1483086603-scaled-1.jpg",
+      "userSince": "2025",
+      "channel": "dogs"
+      }
 
   },
      {
@@ -125,7 +210,14 @@ export default function handler(req, res) {
       "location": "Rhode Island",
       "pic": "https://t3.ftcdn.net/jpg/03/37/46/98/360_F_337469861_iFRwd4Ia15Ihuwfa8fEDA9OKPhIVsgZR.jpg",
       "datePosted": "posted 2 days ago",
-      "caption": "corgi jumping in the air"
+      "caption": "corgi jumping in the air",
+
+      "author": {
+      "name": "corgi379",
+      "image": "https://t3.ftcdn.net/jpg/03/37/46/98/360_F_337469861_iFRwd4Ia15Ihuwfa8fEDA9OKPhIVsgZR.jpg",
+      "userSince": "2013",
+      "channel": "dogs"
+      }
 
   },
      {
@@ -135,7 +227,14 @@ export default function handler(req, res) {
       "location": "Maryland",
       "pic": "https://www.shutterstock.com/image-photo/cavalier-king-charles-spaniel-dog-600nw-2713935839.jpg",
       "datePosted": "posted 4 days ago",
-      "caption": "relaxing"
+      "caption": "relaxing",
+
+      "author": {
+      "name": "KingCharlesSpaniel101",
+      "image": "https://images.pexels.com/photos/16903529/pexels-photo-16903529/free-photo-of-brown-and-white-puppy.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      "userSince": "2017",
+      "channel": "dogs"
+      }
 
   },
      {
@@ -145,7 +244,14 @@ export default function handler(req, res) {
       "location": "North Carolina",
       "pic": "https://media.istockphoto.com/id/92009449/photo/cocker-spaniel.jpg?s=612x612&w=0&k=20&c=cbIC_ZR73ZPwkkn5UfrDrz8bUGM-L2JKS10G_O4tFTU=",
       "datePosted": "posted 4 days ago",
-      "caption": "cocker spaniel laying down"
+      "caption": "cocker spaniel laying down",
+
+      "author": {
+      "name": "cockerspaniellover2015",
+      "image": "https://t3.ftcdn.net/jpg/02/75/98/22/360_F_275982235_xzqfvupzoy03oAiWf7iFTpTcpvVaR4TF.jpg",
+      "userSince": "2015",
+      "channel": "dogs"
+      }
 
   }
   ]); 
